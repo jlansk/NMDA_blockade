46a43466102c31a98d79cdf5db51c62a208e0986
jlansk <155968035+jlansk@users.noreply.github.com>
2024-03-01 13:34:26 +0000

    Add files via upload

---
+++ scripts/dcm_cmm_ipc_gen.m
@@ -0,0 +1,64 @@
+function DCM = dcm_cmm_ipc_gen(DCMa, raw, ana, sub, rawlast)
+% Specifying DCM options
+%==========================================================================       
+%--------------------------------------------------------------------------
+%% ---- Set A and C matrices of DCM from DCMa       
+DCM = DCMa; %specifies DCM.A, DCM.C and DCM.name (which will be overwritten!)
+
+% Data filename
+%--------------------------------------------------------------------------
+
+DCM.xY.Dfile =  [raw filesep sub rawlast];
+
+%% ----DCM model parameters ----
+% Parameters and options used for setting up model
+%--------------------------------------------------------------------------
+DCM.options.analysis = 'ERP'; % analyze evoked responses
+DCM.options.model    = 'CMM_NMDA' %'CMM_NMDA'; % CMC, TFM
+DCM.options.Tdcm(1)  = 0;     % start of peri-stimulus time to be modelled
+DCM.options.Tdcm(2)  = 300;   % end of peri-stimulus time to be modelled
+DCM.options.Nmodes   = 8;     % nr of modes for data selection
+DCM.options.h        = 1;     % nr of DCT components
+DCM.options.onset    = 60;    % selection of onset (prior mean)
+DCM.options.dur     = 16;
+DCM.options.D        = 1;     % downsampling
+DCM.options.trials   = [1 6]  % index of ERPs within file
+
+DCM.xY.modality='MEGPLANAR';
+DCM.options.spatial  = 'ECD'; %LFP 
+
+DCM.options.location = 1;     % optimising source location
+DCM.options.han      = 1;     % applying hanning window
+DCM.options.symmetry=1;
+
+DCM.Lpos = [[-58; -27; 30] [59; -41; 30]]; 
+DCM.Sname  = {'left IPC'; 'right IPC'};
+Nareas    = size(DCM.Lpos,2);
+
+
+
+% Specify B matrix
+%--------------------------------------------------------------------------
+DCM.B{1} = DCM.A{1} + DCM.A{2} + DCM.A{3};  % model specification
+DCM.B{1}=logical(DCM.B{1});
+
+        
+% Between trial effects
+%--------------------------------------------------------------------------
+% Trials defined as deviant standard
+%% ---- Between-trial effects ----
+
+DCM.xU.X(:,1) = [1; 0]; 
+DCM.xU.name{1} = ('d-r5');
+DCM.xU.dt = 0.002; 
+        
+% Define priors
+%--------------------------------------------------------------------------
+[pE,pC]  = spm_dcm_neural_priors(DCM.A,DCM.B,DCM.C,DCM.options.model);
+DCM.M.pE = pE;
+DCM.M.pC = pC;
+
+DCM.name = [ana filesep 'DCM_' sub '_' DCMa.name];
+       
+end
+
